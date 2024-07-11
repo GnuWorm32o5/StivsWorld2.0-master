99d48516b4d23598401aaf767a6870b5b07176f1
GnuWorm32 <stevanglavaskii@gmail.com>
2024-07-11 14:57:10 +0200

    Webmaster

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,12 @@
+
+
 const contactForm = document.getElementById('contact-form'),
     contactMessage = document.getElementById('contact-message')
 
     const sendEmail = (e) => {
         e.preventDefault()
+
+
     // ServiceID - templteID - #form - publicKey
 emailjs.sendForm('service_9fofyu5','template_k3e8x8m','#contact-form','xiA3xdZnp8_pWItSB')
 .then(() => {
@@ -19,3 +23,5 @@ emailjs.sendForm('service_9fofyu5','template_k3e8x8m','#contact-form','xiA3xdZnp
 }
 
 contactForm.addEventListener('submit', sendEmail)
+
+
